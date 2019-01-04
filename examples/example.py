580f886128d175f5e9d72c49243645a44fa13cca
papu bhattacharya <papu.bhattacharya@synectiks.com>
2019-01-04 15:16:57 +0530

    updated example

--- examples/example.py
+++ examples/example.py
@@ -1,21 +1,13 @@
-import xml.etree.ElementTree as ET
-import xmldataset
-import psycopg2
-from sqlalchemy import create_engine
 import pprint
-import datetime
 import pandas as pd
-from pandas import DataFrame,read_csv
+import xmldataset
+from sqlalchemy.types import VARCHAR
+from odo import odo
+from pandas.io import sql
+from sqlalchemy import create_engine
 ppsetup = pprint.PrettyPrinter(indent=4)
 pp = ppsetup.pprint
-from datetime import datetime
-
-
-
 xml_data = open('Vmax_svpg.xml').read()
-
-
-
 
 # for Vmax_svpg
 
@@ -44,61 +36,63 @@ output = xmldataset.parse_using_profile(xml_data,profile)
 
 df = pd.DataFrame.from_records(output['mydata'],columns=["symid","view_name","view_last_update_time","port_grpname","director","port","port_wwn"])
 # print(df['view_last_update_time'])
-
-
 df.view_last_update_time=pd.to_datetime(df['view_last_update_time'])
-
-
-# dd=df.view_last_update_time
-# print(type(dd))
-
-import csv
-import datetime
-
-import MySQLdb
-conn=MySQLdb.connect('localhost', 'root', 'password', 'mysql')
-print('connected')
-
-cur = conn.cursor()
-
-
-df.to_csv('Vmax_svpg.csv',index=False)
-Location=r'C:\mycode\xmldataset\examples\Vmax_svpg.csv'
-df=read_csv(Location)
-
-with open(Location) as f:
-    reader = csv.reader(f)
-    next(reader)  # Skip the header row.
-    for row in reader:
-        # print(row[2])
-        # a = row[0],row[1],row[2],row[3],row[4],row[5],row[6]
-        # row[2] = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
-
-        # print(type(row[2]))
-        # if row[2] !='':
-        #     c = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
-        # else:
-        #     pass# print(type(a))
-        # cur.execute(
-        #     "INSERT INTO ggg(sid, view_name, last_update_time, port_grpname, director, port, port_wwn) VALUES (%s, %s, %s, %s, %s, %s, %s)",a,c)
-        cur.execute(
-            "INSERT INTO dc(last_update_time)VALUES(%s)",row[2])
-# Creating Tables and inserting data
-##### for Vmax_svpg
-
-# cur.execute("""
-#             CREATE TABLE cc(
-#                 sid VARCHAR(64)  ,
-#                 view_name VARCHAR(256) ,
-#                 last_update_time DATE ,
-#                 port_grpname VARCHAR(256)  ,
-#                 director VARCHAR(16)  ,
-#                 port VARCHAR(8)  ,
-#                 port_wwn VARCHAR(32)
-#                 )
-#                     """)
-
-conn.commit()
+print(df.dtypes)
+engine = create_engine('mysql://root:password@localhost/test')
+with engine.connect() as conn, conn.begin():
+    df.to_sql('vmax', conn, if_exists='replace',dtype={"symid": VARCHAR(256)})
+# df.to_csv('Vmax_svpg.csv',index=False)
+# t = odo('Vmax_svpg.csv', 'mysql+pymysql://root:password@localhost/test::nyc')
+#
+# # dd=df.view_last_update_time
+# # print(type(dd))
+#
+# import csv
+#
+# import MySQLdb
+# conn=MySQLdb.connect('localhost', 'root', 'password', 'mysql')
+# print('connected')
+#
+# cur = conn.cursor()
+#
+#
+# df.to_csv('Vmax_svpg.csv',index=False)
+# Location=r'C:\mycode\xmldataset\examples\Vmax_svpg.csv'
+# df=read_csv(Location)
+#
+# with open(Location) as f:
+#     reader = csv.reader(f)
+#     next(reader)  # Skip the header row.
+#     for row in reader:
+#         # print(row[2])
+#         # a = row[0],row[1],row[2],row[3],row[4],row[5],row[6]
+#         # row[2] = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+#
+#         # print(type(row[2]))
+#         # if row[2] !='':
+#         #     c = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+#         # else:
+#         #     pass# print(type(a))
+#         # cur.execute(
+#         #     "INSERT INTO ggg(sid, view_name, last_update_time, port_grpname, director, port, port_wwn) VALUES (%s, %s, %s, %s, %s, %s, %s)",a,c)
+#         cur.execute(
+#             "INSERT INTO dc(last_update_time)VALUES(%s)",row[2])
+# # Creating Tables and inserting data
+# ##### for Vmax_svpg
+#
+# # cur.execute("""
+# #             CREATE TABLE cc(
+# #                 sid VARCHAR(64)  ,
+# #                 view_name VARCHAR(256) ,
+# #                 last_update_time DATE ,
+# #                 port_grpname VARCHAR(256)  ,
+# #                 director VARCHAR(16)  ,
+# #                 port VARCHAR(8)  ,
+# #                 port_wwn VARCHAR(32)
+# #                 )
+# #                     """)
+#
+# conn.commit()
 
 
 
